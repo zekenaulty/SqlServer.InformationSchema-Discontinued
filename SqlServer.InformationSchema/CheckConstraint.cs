1c552ba20e946694389245a2e3cb611abbe2e8f1
Zeke Naulty <zekenaulty@msn.com>
2016-10-11 23:05:50 -0400

    Made changes to CheckConstraint and added new class for tests.

--- SqlServer.InformationSchema/CheckConstraint.cs
+++ SqlServer.InformationSchema/CheckConstraint.cs
@@ -5,12 +5,30 @@ namespace SqlServer.InformationSchema
 	using System.Data;
 	using System.Data.SqlClient;
 	using System.Linq;
-	
-	public class CheckConstraint
+
+    /// <summary>
+    /// A check constraint in SQL Server allows you to specify a condition on each row in a table.
+    /// </summary>
+    public class CheckConstraint
 	{
+        /// <summary>
+        /// Constraint qualifier.
+        /// </summary>
 		public string ConstraintCatalog {get; internal set;}
+
+        /// <summary>
+        /// Name of the schema to which the constraint belongs. ** Important ** Do not use INFORMATION_SCHEMA views to determine the schema of an object. The only reliable way to find the schema of a object is to query the sys.objects catalog view.
+        /// </summary>
 		public string ConstraintSchema {get; internal set;}
+
+        /// <summary>
+        /// Constraint name.
+        /// </summary>
 		public string ConstraintName {get; internal set;}
+
+        /// <summary>
+        /// Actual text of the Transact-SQL definition statement.
+        /// </summary>
 		public string CheckClause {get; internal set;}
 	}
 }
--- SqlServer.InformationSchema/CheckConstraintFactory.cs
+++ SqlServer.InformationSchema/CheckConstraintFactory.cs
@@ -6,22 +6,34 @@ namespace SqlServer.InformationSchema
 	using System.Data.SqlClient;
 	using System.Linq;
 	
+    /// <summary>
+    /// Factory used to build and populate SqlServer.InformationSchema.CheckContraint(s)
+    /// </summary>
 	public class CheckConstraintFactory
 	{
-		public static List<SqlServer.InformationSchema.CheckConstraint> FindAll(System.Data.SqlClient.SqlConnection connection)
+
+        /// <summary>
+        /// Read all INFORMATION_SCHEMA.CHECK_CONSTRAINTS from the in passed SqlServerConnection.
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns>List<CheckConstraint></returns>
+		public static List<CheckConstraint> FindAll(SqlConnection connection)
 		{
 			if ((connection == null))
-			{
-				throw new System.Exception("Connection can not be null/Nothing.");
-			}
+				throw new Exception("Connection can not be null/Nothing.");
+
 			if ((connection.State != ConnectionState.Open))
-			{
 				connection.Open();
-			}
-			System.Data.SqlClient.SqlDataReader reader = null;
+
+			SqlDataReader reader = null;
+
 			try
 			{
-				reader = SqlDb.ExecuteReader(connection, CommandType.Text, "SELECT * FROM INFORMATION_SCHEMA.CHECK_CONSTRAINTS", false);
+				reader = SqlDb.ExecuteReader(
+                    connection, 
+                    CommandType.Text, 
+                    "SELECT * FROM INFORMATION_SCHEMA.CHECK_CONSTRAINTS");
+
 				return ReadRecords(reader);
 			}
 			finally
@@ -33,68 +45,131 @@ namespace SqlServer.InformationSchema
 				}
 			}
 		}
-		public static List<SqlServer.InformationSchema.CheckConstraint> ReadRecords(System.Data.SqlClient.SqlDataReader reader)
+
+        /// <summary>
+        /// Read INFORMATION_SCHEMA.CHECK_CONSTRAINTS from the in passed SqlServerConnection for a specific schema.
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns>List<CheckConstraint></returns>
+		public static List<CheckConstraint> FindBySchema(SqlConnection connection, string schema)
+        {
+            if ((connection == null))
+                throw new Exception("Connection can not be null/Nothing.");
+
+            if ((connection.State != ConnectionState.Open))
+                connection.Open();
+
+            SqlDataReader reader = null;
+
+            try
+            {
+                reader = SqlDb.ExecuteReader(
+                    connection, 
+                    CommandType.Text, 
+                    "SELECT * FROM INFORMATION_SCHEMA.CHECK_CONSTRAINTS WHERE CONSTRAINT_SCHEMA = @schema", 
+                    new SqlParameter("@schema", schema));
+
+                return ReadRecords(reader);
+            }
+            finally
+            {
+                if ((reader != null))
+                {
+                    reader.Close();
+                    reader = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Read INFORMATION_SCHEMA.CHECK_CONSTRAINTS from the in passed SqlServerConnection for a specific schema and name.
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns>List<CheckConstraint></returns>
+		public static List<CheckConstraint> FindByName(SqlConnection connection, string schema, string name)
+        {
+            if ((connection == null))
+                throw new Exception("Connection can not be null/Nothing.");
+
+            if ((connection.State != ConnectionState.Open))
+                connection.Open();
+
+            SqlDataReader reader = null;
+
+            try
+            {
+                reader = SqlDb.ExecuteReader(
+                    connection, 
+                    CommandType.Text, 
+                    "SELECT * FROM INFORMATION_SCHEMA.CHECK_CONSTRAINTS WHERE CONSTRAINT_SCHEMA = @schema AND CONSTRAINT_NAME = @constraintName", 
+                    new SqlParameter("@schema", schema), 
+                    new SqlParameter("@constraintName", name));
+
+                return ReadRecords(reader);
+            }
+            finally
+            {
+                if ((reader != null))
+                {
+                    reader.Close();
+                    reader = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Read records from an in passed SqlDataReader into a list of SqlServer.InformationSchema.CheckConstraint(s)
+        /// </summary>
+        /// <param name="reader">SqlDataReader</param>
+        /// <returns>List<CheckConstraint></returns>
+		public static List<CheckConstraint> ReadRecords(SqlDataReader reader)
 		{
 			if ((reader == null))
-			{
-				throw new System.Exception("Reader can not be null/Nothing.");
-			}
+				throw new Exception("Reader can not be null/Nothing.");
+
 			if ((reader.HasRows == false))
-			{
-				throw new System.Exception("Reader has no rows.");
-			}
-			List<SqlServer.InformationSchema.CheckConstraint> result = null;
+				throw new Exception("Reader has no rows.");
+
+			List<CheckConstraint> result = null;
+
 			int ConstraintCatalogOrdinal = reader.GetOrdinal("CONSTRAINT_CATALOG");
 			int ConstraintSchemaOrdinal = reader.GetOrdinal("CONSTRAINT_SCHEMA");
 			int ConstraintNameOrdinal = reader.GetOrdinal("CONSTRAINT_NAME");
 			int CheckClauseOrdinal = reader.GetOrdinal("CHECK_CLAUSE");
-			for (
-			; reader.Read(); 
-			)
+
+			while (reader.Read())
 			{
 				if ((result == null))
-				{
-					result = new List<SqlServer.InformationSchema.CheckConstraint>();
-				}
-				SqlServer.InformationSchema.CheckConstraint tmp = new SqlServer.InformationSchema.CheckConstraint();
+					result = new List<CheckConstraint>();
+
+				CheckConstraint tmp = new CheckConstraint();
+
 				if (reader.IsDBNull(ConstraintCatalogOrdinal))
-				{
 					tmp.ConstraintCatalog = String.Empty;
-				}
 				else
-				{
 					tmp.ConstraintCatalog = reader.GetString(ConstraintCatalogOrdinal);
-				}
+
 				if (reader.IsDBNull(ConstraintSchemaOrdinal))
-				{
 					tmp.ConstraintSchema = String.Empty;
-				}
 				else
-				{
 					tmp.ConstraintSchema = reader.GetString(ConstraintSchemaOrdinal);
-				}
+
 				if (reader.IsDBNull(ConstraintNameOrdinal))
-				{
 					tmp.ConstraintName = String.Empty;
-				}
 				else
-				{
 					tmp.ConstraintName = reader.GetString(ConstraintNameOrdinal);
-				}
+
 				if (reader.IsDBNull(CheckClauseOrdinal))
-				{
 					tmp.CheckClause = String.Empty;
-				}
 				else
-				{
 					tmp.CheckClause = reader.GetString(CheckClauseOrdinal);
-				}
+
 				result.Add(tmp);
 			}
-			if ((result == null))
-			{
+
+			if (result == null)
 				return new List<SqlServer.InformationSchema.CheckConstraint>();
-			}
+
 			return result;
 		}
 	}
