7337a0f274fa7db00e39f8d1e3bcb9d06dcdd926
Zeke Naulty <zekenaulty@msn.com>
2016-10-12 00:30:02 -0400

    Completed CHECK_CONSTRAINTS tests

--- SqlServer.InformationSchema.Test/Program.cs
+++ SqlServer.InformationSchema.Test/Program.cs
@@ -1,16 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SqlServer.InformationSchema.Test
 {
+    /// <summary>
+    /// Because an actual database is needed to test these classes I have chosen to use a simple console application instead of using Unit tests.
+    /// These tests will be based on the AdventureWorks sample database: https://www.microsoft.com/en-us/download/details.aspx?id=49502
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
 
+            Console.BufferWidth = 3000;
+            Console.BufferHeight = 10000;
+            Console.WindowWidth = Console.LargestWindowWidth - 100;
+            Console.WindowHeight = Console.LargestWindowHeight - 40;
+
+            Console.WriteLine("SqlServer.InformationSchema Test(s)");
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["test"].ConnectionString))
+            {
+                #region CHECK_CONSTRAINTS
+                if(RunTestSection("CHECK_CONSTRANTS"))
+                {
+                    TestCheckConstraints.FindByName(connection, "Production", "CK_Product_ProductLine");
+                    EnterForNext();
+
+                    TestCheckConstraints.FindBySchema(connection, "Purchasing");
+                    EnterForNext();
+
+                    if(RunTestSection("CHECK_CONSTRAINT FindAll"))
+                    {
+                        TestCheckConstraints.FindAll(connection);
+                        EnterForNext();
+                    }
+                }
+                #endregion
+            }
+
+            Console.Write("Press ENTER to exit.");
+            WaitForEnter();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static void EnterForNext()
+        {
+            Console.Write("Press ENTER to run next test.");
+            WaitForEnter();
+            Console.Clear();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="section"></param>
+        /// <returns></returns>
+        static bool RunTestSection(string section)
+        {
+            Console.Clear();
+            Console.Write($"Press Y to run {section} tests N to skip.");
+
+            ConsoleKey key = Console.ReadKey(true).Key;
+            while(key != ConsoleKey.Y && key != ConsoleKey.N)
+            {
+                key = Console.ReadKey(true).Key;
+            }
+
+            Console.WriteLine();
+
+            if (key == ConsoleKey.Y) return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static void WaitForEnter()
+        {
+            ConsoleKey key = Console.ReadKey(true).Key;
+            while (key != ConsoleKey.Enter)
+            {
+                key = Console.ReadKey(true).Key;
+            }
+
+            Console.WriteLine();
         }
     }
 }
--- SqlServer.InformationSchema.Test/TestCheckConstraints.cs
+++ SqlServer.InformationSchema.Test/TestCheckConstraints.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,44 @@ namespace SqlServer.InformationSchema.Test
 {
     class TestCheckConstraints
     {
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="constraints"></param>
+        private static void EmitCheckConstrants(List<CheckConstraint> constraints)
+        {
+            for (int i = 0; i < constraints.Count; i++)
+                Utility.EmitProperties(constraints[i]);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connection">SqlConnection</param>
+        internal static void FindAll(SqlConnection connection)
+        {
+            Console.WriteLine("CheckConstraintFactory.FindAll");
+            EmitCheckConstrants(CheckConstraintFactory.FindAll(connection));         
+        }
+
+        internal static void FindBySchema(SqlConnection connection, string schema)
+        {
+            Console.WriteLine($"CheckConstraintFactory.FindSchema - {schema}");
+            EmitCheckConstrants(CheckConstraintFactory.FindBySchema(connection, schema));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="schema"></param>
+        /// <param name="name"></param>
+        internal static void FindByName(SqlConnection connection, string schema, string name)
+        {
+            Console.WriteLine($"CheckConstraintFactory.FindSchema - {schema}.{name}");
+            EmitCheckConstrants(CheckConstraintFactory.FindByName(connection, schema, name));
+        }
+
     }
 }
